a1820bec7d16789a9ce69f8725b86777dfaf0cc5
Emelie von Schantz <emelie@MacBook-Pro-som-tillhor-Emelie.local>
2025-10-13 18:13:59 +0200

    Add main class and method

---
+++ main.py
@@ -0,0 +1,11 @@
+from data_preparation import PrepareData
+from data_modeling_and_model_evalutation import ModelAndEvaluateData
+from data_exploration import ExploreData 
+
+def main():
+    PrepareData()
+    ModelAndEvaluateData()
+    ExploreData()
+
+if __name__ == "__main__":
+    main()
