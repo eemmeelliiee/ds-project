0712ac6c8459d5f6f8d3e9b0d28fe3e570718a72
Emelie von Schantz <emelie@MacBook-Pro-som-tillhor-Emelie.local>
2025-10-13 18:40:37 +0200

    Wrap scripts in methods that are run through main.py

--- data_modeling_and_model_evalutation.py
+++ data_modeling_and_model_evalutation.py
@@ -12,74 +12,76 @@ from sklearn.metrics import precision_score
 from pathlib import Path
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, classification_report
 
-# Validate input file(s)
-def ReadPreviousData(location: str):
-    Filepath = Path(location)
-    if Filepath.exists():
-        data = pd.read_csv(location) 
-        return data, True 
-    else:
-        data = pd.DataFrame()
+def ModelAndEvaluateData():
 
-df, df_exists = ReadPreviousData('data/cleaned_dataset.csv')
+    # Validate input file(s)
+    def ReadPreviousData(location: str):
+        Filepath = Path(location)
+        if Filepath.exists():
+            data = pd.read_csv(location) 
+            return data, True 
+        else:
+            data = pd.DataFrame()
+            return data, False
 
-PreviousDataComplete = all([df_exists])
+    df, df_exists = ReadPreviousData('data/cleaned_dataset.csv')
 
-if PreviousDataComplete == False:
-    sys.exit("ERROR: Not all required data found.") 
-print("All files found")
+    PreviousDataComplete = all([df_exists])
 
-# Define features/target
-X = df.drop(columns='water_quality')
-y = df['water_quality']
+    if PreviousDataComplete == False:
+        sys.exit("ERROR: Not all required data found.") 
+    print("All files found")
 
-# Split dataset for final test
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
-clf = DecisionTreeClassifier(random_state=42, class_weight='balanced')
+    # Define features/target
+    X = df.drop(columns='water_quality')
+    y = df['water_quality']
 
-# Cross validation
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-f1_macro = make_scorer(f1_score, average='macro')
-cv_acc_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring='accuracy')
-cv_f1_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring=f1_macro)
-# Print mean of cv scores
-print(f"Avg CV accuracy score: {cv_acc_scores.mean():.4f}")
-print(f"Avg CV F1 score: {cv_f1_scores.mean():.4f}")
+    # Split dataset for final test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
+    clf = DecisionTreeClassifier(random_state=42, class_weight='balanced')
 
-# Test on test set
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
+    # Cross validation
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    f1_macro = make_scorer(f1_score, average='macro')
+    cv_acc_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring='accuracy')
+    cv_f1_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring=f1_macro)
+    # Print mean of cv scores
+    print(f"Avg CV accuracy score: {cv_acc_scores.mean():.4f}")
+    print(f"Avg CV F1 score: {cv_f1_scores.mean():.4f}")
 
-test_acc_score = accuracy_score(y_test, y_pred)
-test_f1_macro = f1_score(y_test, y_pred, average='macro')
-test_f1_weighted = f1_score(y_test, y_pred, average='weighted')
-test_recall_macro = recall_score(y_test, y_pred, average='macro')
-test_precision_macro = precision_score(y_test, y_pred, average='macro')
+    # Test on test set
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
 
-print(f"Avg test accuracy score:     {test_acc_score:.4f}")
-print(f"Avg test F1 (macro):         {test_f1_macro:.4f}")
-print(f"Avg test F1 (weighted):      {test_f1_weighted:.4f}")
-print(f"Avg test recall (macro):     {test_recall_macro:.4f}")
-print(f"Avg test precision (macro):  {test_precision_macro:.4f}")
+    test_acc_score = accuracy_score(y_test, y_pred)
+    test_f1_macro = f1_score(y_test, y_pred, average='macro')
+    test_f1_weighted = f1_score(y_test, y_pred, average='weighted')
+    test_recall_macro = recall_score(y_test, y_pred, average='macro')
+    test_precision_macro = precision_score(y_test, y_pred, average='macro')
 
-# Confusion matrix
-cm=confusion_matrix(y_test, y_pred)
-print(cm)
-cm_heatmap = sns.heatmap(cm, annot=True)
-fig = cm_heatmap.get_figure()
-fig.savefig('figures/confusion_matrix')
+    print(f"Avg test accuracy score:     {test_acc_score:.4f}")
+    print(f"Avg test F1 (macro):         {test_f1_macro:.4f}")
+    print(f"Avg test F1 (weighted):      {test_f1_weighted:.4f}")
+    print(f"Avg test recall (macro):     {test_recall_macro:.4f}")
+    print(f"Avg test precision (macro):  {test_precision_macro:.4f}")
 
-# Save decision tree
-fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=300)
-tree.plot_tree(clf,filled=True,rounded=True, ax=axes)
-plt.savefig('figures/decision_tree.png')
+    # Confusion matrix
+    cm=confusion_matrix(y_test, y_pred)
+    print(cm)
+    cm_heatmap = sns.heatmap(cm, annot=True)
+    cm_heatmap.get_figure().savefig('figures/confusion_matrix')
 
-# #_------------------------------
-# # # Multiclass proof
-# # print(clf.classes_)
-# # print(len(clf.classes_))
+    # Save decision tree
+    fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=300)
+    tree.plot_tree(clf,filled=True,rounded=True, ax=axes)
+    plt.savefig('figures/decision_tree.png')
+
+    # #_------------------------------
+    # # # Multiclass proof
+    # # print(clf.classes_)
+    # # print(len(clf.classes_))
 
 
-# # Classification report
-# cp = classification_report(y_test, y_pred)
-# print('\nClassification report:\n', cp)
+    # # Classification report
+    # cp = classification_report(y_test, y_pred)
+    # print('\nClassification report:\n', cp)
